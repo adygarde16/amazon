17b1bec13f43b39cc3cf9f0e3e99dec6bcf3dd7a
ady16garde <adhigarde16e@student.mes.ac.in>
2021-12-23 11:17:58 +0530

    Added date of birth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	require 'date'
+	
 	protected
 
 	def configure_permitted_parameters
@@ -11,4 +13,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
 		
 	end
+
+    def age(dob)
+    now = Time.now.utc.to_date
+    snow.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,9 @@
 class UserController < ApplicationController
   
   before_action :authenticate_user!
-  def index
-  end
+  require 'date'
+  def age(dob)
+  now = Time.now.utc.to_date
+  now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+end
 end
